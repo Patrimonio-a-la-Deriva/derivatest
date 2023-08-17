88ceffacf8785cef6469444a2c1030a24b2bed4e
Jonathan <95005445+JonNauto@users.noreply.github.com>
2023-08-17 14:40:57 -0400

    v9

--- Aladeriva/boton2.js
+++ Aladeriva/boton2.js
@@ -1,16 +1,22 @@
 const d = document;
 
-export default function Boton2(pr, selector) {
+export default function Boton2(pr, selector, modal) {
     const $pr = d.querySelector(pr),
-        $select = d.querySelector(selector);
+        $select = d.querySelector(selector),
+        $modal = d.querySelector(modal);
     d.addEventListener('mouseover', (e) => {
         if (e.target.matches(pr)) {
             $select.textContent = 'Santiago Centro'
         }
-        else{
+        else {
             $select.textContent = ''
         }
     })
 
+    d.addEventListener('click', (e) => {
+        if (e.target.matches(pr)) {
+            $modal.classList.remove('none')
+        }
+    })
 }
 
